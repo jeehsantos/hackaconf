bf29068c8de8da6069b7fae30735f33f0054420d
Jefferson dos Santos <idreamzjsm@gmail.com>
2018-07-19 18:16:13 -0300

    up button scroll added

--- js/index.js
+++ js/index.js
@@ -15,3 +15,21 @@ ativaScrollSuave('a[href*=header]');
 ativaScrollSuave('a[href*=panel-about]');
 ativaScrollSuave('a[href*=panel-speakers]');
 ativaScrollSuave('a[href*=panel-form]');
+
+window.onscroll = function() {
+	ativaScrollButton()
+
+};
+
+function ativaScrollButton() {
+    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+        document.getElementById("scroll-button").style.display = "block";
+    } else {
+        document.getElementById("scroll-button").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
